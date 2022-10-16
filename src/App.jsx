b11b3189cd24ec68b439f78f846bea45456b0041
lyx <yxianlai@hotmail.com>
2022-10-16 12:31:09 +0800

    test router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
-import Button from "./components/Button";
+function Home() {
+  return (
+    <div>
+      <h1>This is the home page</h1>
+      <Link to="about">Click to view our about page</Link>
+      <br />
+      <Link to="contact">Click to view our contact page</Link>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>This is the about page</h1>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h1>This is the contact page</h1>
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <h1>This is Heading 1.</h1>
-        <Button />
-        <p>I am a paragraph.</p>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={Home()} />
+            <Route path="about" element={About()} />
+            <Route path="contact" element={Contact()} />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
