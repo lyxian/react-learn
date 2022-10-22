799e13591f3c77b7a1368f23d6173e6565831ec9
lyx <yxianlai@hotmail.com>
2022-10-23 02:33:29 +0800

    to-do-list-1 - impl taskList, taskForm

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,56 @@ import "./App.css";
 
 import Button from "./components/Button";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <h1>This is Heading 1.</h1>
-        <Button />
-        <p>I am a paragraph.</p>
+// function Task(props) {
+function Task({ props }) {
+  // console.log(props);
+  return <div className="todo">{props.text}</div>;
+}
+
+function TaskForm({ addTask }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTask(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App() {
+  const [tasks, setTasks] = React.useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" },
+  ]);
+
+  const addTask = (text) => {
+    const newTasks = [...tasks, { text }];
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {tasks.map((task, index) => (
+          <Task key={index} index={index} props={task} />
+        ))}
+        <TaskForm addTask={addTask} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
