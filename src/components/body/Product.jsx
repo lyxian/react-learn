daaabbda7048fadd8aac96fb832e5a41795bedb8
lyx <yxianlai@hotmail.com>
2023-01-08 16:47:01 +0800

    food-store - add product css

--- src/components/body/Product.jsx
+++ src/components/body/Product.jsx
@@ -1,7 +1,41 @@
 import React, { Component } from "react";
 import "./Product.css";
+import { products } from "../../data/products";
+
+function Card(props) {
+  const image =
+    props.product.image === "NA" ? (
+      <div>{props.product.image}</div>
+    ) : (
+      <img src={props.product.image} />
+    );
+  return (
+    <div className="card">
+      <a>
+        <div className="card-inner">
+          <div className="card-left">{image}</div>
+          <div className="card-right">
+            <div className="card-right-top">
+              <h3>{props.product.title}</h3>
+              <div>{props.product.description}</div>
+            </div>
+            <div className="card-right-btm">
+              <h3>S${props.product.price}</h3>
+              <button>
+                <h4>Add</h4>
+              </button>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+}
 
 function Product(props) {
+  const productCards = products.map((product, index) => (
+    <Card key={index} product={product} />
+  ));
   const productCategory = props.category ? props.category : "<Category>";
   const productDescription = props.Description
     ? props.Description
@@ -12,53 +46,7 @@ function Product(props) {
         <div>{productCategory}</div>
         <div>{productDescription}</div>
       </div>
-      <div className="productCards">
-        <div className="card">
-          <div className="card-inner">
-            <div>Title</div>
-            <div>Image</div>
-            <div>Description</div>
-            <div>Price</div>
-            <button>Add</button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-inner">
-            <div>Title</div>
-            <div>Image</div>
-            <div>Description</div>
-            <div>Price</div>
-            <button>Add</button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-inner">
-            <div>Title</div>
-            <div>Image</div>
-            <div>Description</div>
-            <div>Price</div>
-            <button>Add</button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-inner">
-            <div>Title</div>
-            <div>Image</div>
-            <div>Description</div>
-            <div>Price</div>
-            <button>Add</button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-inner">
-            <div>Title</div>
-            <div>Image</div>
-            <div>Description</div>
-            <div>Price</div>
-            <button>Add</button>
-          </div>
-        </div>
-      </div>
+      <div className="productCards">{productCards}</div>
     </React.Fragment>
   );
 }
---
+++ src/data/products.js
@@ -0,0 +1,15 @@
+export const products = [
+    { title: "Pizza", image: "NA", description: "NA", price: 1 },
+    { title: "Pasta", image: "https://ik.imagekit.io/jq1luxum6oz/https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/TruffleMushroomLinguinee47d0c.jpg?tr=w-250,h-250", description: "Sauteed mix mushrooms, porcini cream sauce, white truffle oil, parmesan shavings, De Checco linguine. ", price: 26 },
+    {
+        title: "Fries",
+        image:
+            "https://ik.imagekit.io/jq1luxum6oz/https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/TruffleFriesMaximus358243.jpg?tr=w-250,h-250",
+        description: "Thick cut fries, sautéed mushrooms with thyme, white truffle oil,  parmesan cheese, truffle mayo.",
+        price: 19,
+    },
+    { title: "Chick", image: "https://ik.imagekit.io/jq1luxum6oz/https://s3-ap-southeast-1.amazonaws.com/v3-live.image.oddle.me/product/BuffaloWings7b0d58.jpg?tr=w-250,h-250", description: "Crispy chicken wings coated with sweet, spicy, and tangy buffalo sauce. Comes with ranch dip. ", price: 9 },
+    { title: "Salad", image: "NA", description: "NA", price: 1 },
+    { title: "Wagyu", image: "NA", description: "NA", price: 1 },
+    { title: "Drink", image: "NA", description: "NA", price: 1 },
+];
